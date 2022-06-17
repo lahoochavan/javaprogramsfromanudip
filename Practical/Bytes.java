c048b061f8d747e7559e07b2a9d4a328afe29e45
lahoochavan <107389011+lahoochavan@users.noreply.github.com>
2022-06-17 23:21:03 +0530

    Add files via upload

---
+++ Practical/Bytes.java
@@ -0,0 +1,16 @@
+package SwitchX;
+
+public class Bytes {
+
+	public static void main(String[] args) {
+		
+		byte b1,b2;
+		
+		b1=-128;
+		b2=67;
+		
+	    System.out.println("Byte1 : "+b1);
+		System.out.println("Byte2 : "+b2);
+	}
+
+}
