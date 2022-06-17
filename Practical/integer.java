872e74aaaaddecb37c543fc23135edddd03b5d11
lahoochavan <107389011+lahoochavan@users.noreply.github.com>
2022-06-17 23:40:36 +0530

    Add files via upload

---
+++ Practical/integer.java
@@ -0,0 +1,14 @@
+package SwitchX;
+
+public class integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x,y;
+x=123;
+y=78;
+System.out.println("Kmph: "+y);
+System.out.println("Speed: "+x);
+	}
+
+}
