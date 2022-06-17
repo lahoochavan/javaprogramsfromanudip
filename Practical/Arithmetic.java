bd30b75e1c77c7ae5b87e46f120626991822df06
lahoochavan <107389011+lahoochavan@users.noreply.github.com>
2022-06-17 23:23:01 +0530

    Add files via upload

---
+++ Practical/Arithmetic.java
@@ -0,0 +1,39 @@
+	package SwitchX;
+	
+	public class Arithmetic {
+	
+		public static void main(String[] args) {
+			
+		// Addition
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println("a+b : "+c);
+		
+		//Multiplication
+		int x = 10;
+		int y = 5;
+		int z = x*y;
+		System.out.println("x*y : "+z);
+		
+		//Division
+		int q = 10;
+		int r = 5;
+		int s = q/r;
+		System.out.println("q/r : "+s);
+		
+		//Subtraction
+		int h = 10;
+		int i = 5;
+		int j = h-i;
+		System.out.println("h-i : "+j);
+		
+		//Modulo
+		int d = 10;
+		int e = 5;
+		int f = d%e;
+		System.out.println("d%e : "+f);
+		
+		}
+	
+	}
