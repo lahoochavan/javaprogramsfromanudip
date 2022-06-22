8a3185cde3c372e4ad78ff80aec988cb8768f576
lahoochavan <107389011+lahoochavan@users.noreply.github.com>
2022-06-22 13:54:29 +0530

    Add files via upload

---
+++ Practical/SwitchVowel.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class SwitchVowel {
+
+	public static void main(String[] args) {
+
+		char a = 'z';
+		switch (a) {
+		case 'a':
+			System.out.println("vowel");
+			break;
+
+		case 'b':
+			System.out.println("vowel");
+			break;
+
+		case 'c':
+			System.out.println("vowel");
+			break;
+
+		case 'd':
+			System.out.println("vowel");
+			break;
+
+		case 'e':
+			System.out.println("vowel");
+			break;
+
+		default:
+			System.out.println("None of the above");
+		}
+	}
+
+}
